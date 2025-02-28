50513326155a6b795e60ad02faac02ac36dccda0
vijay <pavankumarp5727@gmail.com>
2025-02-28 15:48:27 +0530

    setings changes

--- src/app/componets/dashbord/settings/settings.component.ts
+++ src/app/componets/dashbord/settings/settings.component.ts
@@ -41,7 +41,7 @@ export class SettingsComponent extends BaseComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   isAddEdt = false; aeTyp = 'a'; playersList: any; editData: any;
   adonai = false; crm = false; userLst: any;
-  submitted = false; userData: any;
+  submitted = false; userData: any; roleid: any;
   // userForm!: FormGroup;
   cnfmPaswrd: any = ''; paswrd: any = '';
   adoanAiRole: any; todayDt = new Date();
