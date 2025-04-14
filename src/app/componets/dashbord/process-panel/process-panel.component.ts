9674b1344929bb2910427803295f1a0e996bac88
vijay <pavankumarp5727@gmail.com>
2025-04-14 16:17:39 +0530

    optimization tables

--- src/app/componets/dashbord/process-panel/process-panel.component.ts
+++ src/app/componets/dashbord/process-panel/process-panel.component.ts
@@ -1,12 +1,229 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { NgbNavModule, NgbDropdownModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { SharedModule } from '../../../shared/common/sharedmodule';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule, } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SwitherService } from '../../../shared/services/swither.service';
+import { ToastrService } from 'ngx-toastr';
+import { BaseComponent } from '../../../shared/base/base.component';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MaterialModuleModule } from '../../../material-module/material-module.module';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
 
 @Component({
   selector: 'app-process-panel',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, NgbNavModule, NgbDropdownModule, NgSelectModule, ReactiveFormsModule,
+    CommonModule, MatFormFieldModule, MatSelectModule, MaterialModuleModule,
+    MatPaginator, MatPaginatorModule, MatCheckboxModule, MatSort, MatSortModule, MatTableModule, OverlayscrollbarsModule
+  ],
   templateUrl: './process-panel.component.html',
   styleUrl: './process-panel.component.scss'
 })
-export class ProcessPanelComponent {
+export class ProcessPanelComponent extends BaseComponent{
+  processPanelDisplayedColumn: string[] = ['code', 'name', 'panel', 'skin1', 'skin2', 'isActive', 'edit', 'delete'];
+  processPanelDataSource = new MatTableDataSource<any>();
+  @ViewChild('processPanelPaginator') processPanelPaginator!: MatPaginator;
 
+  userDataStorage = localStorage.getItem('userDetails');
+  userData: any = this.userDataStorage ? JSON.parse(this.userDataStorage) : null;
+  userEmail: string = this.userData ? this.userData.email : '';
+  userName: string = this.userData ? this.userData.username : '';
+  userCompanyCode: string = this.userData ? this.userData.companyCode : '';
+  userType: string = this.userData ? this.userData.type : '';
+  processPanelForm!: FormGroup; panelItems: any[] = []; skinItems: any[] = [];
+
+  public processPanelSubmitted = false;
+
+  constructor(private modalService: NgbModal, private fb: FormBuilder, public switchService: SwitherService, private toastr: ToastrService,
+  ) {
+    super();
+
+  }
+  ngOnInit(): void {
+    this.getProcessPanelData();
+    this.getPanelData();
+    this.getSkinData();
+
+    //process panel form
+    this.processPanelForm = this.fb.group({
+      processedPanelId: [{ value: this.generateProductId(), disabled: true }],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      panel: ['', Validators.required],
+      skin1: ['', Validators.required],
+      skin2: ['', Validators.required],
+      pricePerFt: ['', Validators.required],
+      gst: [0, Validators.required],
+      finalAmount: [0, Validators.required],
+      isActive: [true],
+      companyCode: [this.userCompanyCode],
+      email: [this.userEmail],
+      type: [this.userType],
+    });
+  }
+  
+  onProcessPanelSubmit(modal: any) {
+    this.processPanelSubmitted = true;
+    if (this.processPanelForm.invalid) {
+      this.toastr.error("Please fill in all required fields.");
+      return;
+    }
+    let payload = {
+      ...this.processPanelForm.value,
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    this.switchService.saveProcessPanelData(payload).subscribe({
+      next: (res: any) => {
+        if (res.status === true) {
+          this.toastr.success(res.message);
+          if (modal) {
+            this.modalService.dismissAll(modal);
+          }
+          this.processPanelForm.reset();
+          this.processPanelSubmitted = false;
+          this.getProcessPanelData();
+        } else {
+          this.toastr.error(res.message);
+        }
+      },
+      error: (error) => {
+        this.toastr.error(error.statusText || "An error occurred while saving the product.");
+      }
+    });
+  }
+
+  getPanelData() {
+    let payload = {
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    this.switchService.displayPanelData(payload).subscribe({
+      next: (res: any) => {
+        this.panelItems = res;
+      },
+      error: (error) => {
+        this.toastr.error("Error fetching product data");
+      }
+    });
+  }
+
+  getSkinData() {
+    let payload = {
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    this.switchService.displaySkinData(payload).subscribe({
+      next: (res: any) => {
+        this.skinItems = res;
+      },
+      error: (error) => {
+        this.toastr.error("Error fetching skin data");
+      }
+    });
+  }
+
+  getProcessPanelData() {
+    let payload = {
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    this.switchService.displayProcessPanelData(payload).subscribe({
+      next: (res: any) => {
+        this.processPanelDataSource.data = res || [];
+        if (this.processPanelPaginator) {
+          this.processPanelDataSource.paginator = this.processPanelPaginator;
+        }
+      },
+      error: (error) => {
+        this.toastr.error("Error fetching product data");
+        this.processPanelDataSource.data = [];
+      }
+    });
+  }
+
+
+  deleteProcessPanel(data: any) {
+    const prodId = data.processedPanelId;
+    if (!prodId) {
+      alert('Error: Process Panel ID is missing!');
+      return;
+    }
+    if (confirm('Are you sure you want to delete this process panel?')) {
+      this.switchService.deleteProcessPanelData(prodId).subscribe({
+        next: (response) => {
+          this.toastr.success(response.message);
+          this.getProcessPanelData();
+        },
+        error: (error) => {
+          this.toastr.error("id not yed");
+        }
+      });
+    }
+  }
+
+  generateProductId(): number {
+    return Math.floor(1000 + Math.random() * 9000); 
+  }
+
+  ngAfterViewInit() {
+    this.processPanelDataSource.paginator = this.processPanelPaginator;
+  }
+
+  panelGetSNo(index: number): number {
+    if (this.processPanelPaginator && this.processPanelPaginator.pageIndex !== undefined && this.processPanelPaginator.pageSize !== undefined) {
+      return this.processPanelPaginator.pageIndex * this.processPanelPaginator.pageSize + index + 1;
+    }
+    return index + 1; // Default return if paginator is not yet defined
+  }
+
+  panelApplyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.processPanelDataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  getStatusClass(isActive: boolean): string {
+    return isActive ? "badge bg-success-transparent ps-3 fs-11 order-status complete " : "badge bg-danger-transparent ps-3 fs-11 order-status cancel";
+  }
+
+  getStatusText(isActive: boolean): string {
+    return isActive ? "Active" : "Inactive";
+  }
+  allowOnlyNumbers(event: KeyboardEvent): void {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (
+      (charCode >= 48 && charCode <= 57) ||
+      charCode === 46
+    ) {
+      return;
+    }
+    event.preventDefault();
+  }
+
+  openLg26(content30: any) {
+    this.modalService.open(content30, { size: 'sm', scrollable: true, centered: true, });
+  }
+
+  openLg16(content30: any) {
+    this.modalService.open(content30, { scrollable: true, centered: true, });
+  }
+
+  processPanelApplyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.processPanelDataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 }
--- src/app/componets/dashbord/products/products.component.ts
+++ src/app/componets/dashbord/products/products.component.ts
@@ -1,16 +1,185 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NouisliderModule } from 'ng2-nouislider';
-import { NgbCollapseModule, NgbDropdownModule  } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { NgbNavModule, NgbDropdownModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { SharedModule } from '../../../shared/common/sharedmodule';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule, } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SwitherService } from '../../../shared/services/swither.service';
+import { ToastrService } from 'ngx-toastr';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MaterialModuleModule } from '../../../material-module/material-module.module';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [SharedModule, RouterModule, NouisliderModule, NgbCollapseModule, FormsModule, NgbDropdownModule],
+  imports: [SharedModule, NgbNavModule, NgbDropdownModule, NgSelectModule, ReactiveFormsModule,
+    CommonModule, MatFormFieldModule, MatSelectModule, MaterialModuleModule,
+    MatPaginator, MatPaginatorModule, MatCheckboxModule, MatSort, MatSortModule, MatTableModule, OverlayscrollbarsModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  productDisplayedColumn: string[] = ['slNo', 'code', 'name', 'description', 'isActive', 'edit', 'delete'];
+  productDataSource = new MatTableDataSource<any>();
+  @ViewChild('productPaginator') productPaginator!: MatPaginator;
+
+  userDataStorage = localStorage.getItem('userDetails');
+  userData: any = this.userDataStorage ? JSON.parse(this.userDataStorage) : null;
+  userEmail: string = this.userData ? this.userData.email : '';
+  userName: string = this.userData ? this.userData.username : '';
+  userCompanyCode: string = this.userData ? this.userData.companyCode : '';
+  userType: string = this.userData ? this.userData.type : '';
+  isEditingProduct: boolean = false; productForm!: FormGroup;
+
+  public productSubmitted = false;
+
+  constructor(private modalService: NgbModal, private fb: FormBuilder, public switchService: SwitherService, private toastr: ToastrService,
+    private offcanvasService: NgbOffcanvas, private dialog: MatDialog, private cdRef: ChangeDetectorRef) {
+
+  }
+  ngOnInit(): void {
+    this.getProductData();
+
+    this.productForm = this.fb.group({
+      prodId: [{ value: this.generateProductId(), disabled: true }],
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      description: [''],
+      isActive: [true],
+      companyCode: [this.userCompanyCode],
+      email: [this.userEmail],
+      type: [this.userType]
+    });
+    
+  }
+
+  ngAfterViewInit() {
+    this.productDataSource.paginator = this.productPaginator;
+  }
+
+  generateProductId(): number {
+    return Math.floor(1000 + Math.random() * 9000); 
+  }
+
+  onProductSubmit(modal: any) {
+    this.productSubmitted = true;
+    if (this.productForm.invalid) {
+      this.toastr.error("Please fill in all required fields.");
+      return;
+    }
+    let payload = {
+      ...this.productForm.getRawValue(),
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    const request = this.switchService.saveOrUpdateProduct(payload);
+    request.subscribe({
+      next: (res: any) => {
+        if (res.status === true) {
+          const message = this.isEditingProduct
+            ? "Product updated successfully."
+            : "Product saved successfully.";
+          this.toastr.success(message);
+          this.modalService.dismissAll(modal);
+          this.productForm.reset();
+          this.productSubmitted = false;
+          this.isEditingProduct = false;
+          this.getProductData();
+        } else {
+          this.toastr.error(res.message);
+        }
+      },
+      error: (error) => {
+        this.toastr.error(error.statusText || "An error occurred while saving the product.");
+      }
+    });
+  }
+
+  getProductData() {
+    let payload = {
+      email: this.userEmail,
+      companyCode: this.userCompanyCode,
+      type: this.userType
+    };
+    this.switchService.displayProductData(payload).subscribe({
+      next: (res: any) => {
+        this.productDataSource.data = res || [];
+        if (this.productPaginator) {
+          this.productDataSource.paginator = this.productPaginator;
+        }
+      },
+      error: (error) => {
+        this.toastr.error("Error fetching product data");
+        this.productDataSource.data = [];
+      }
+    });
+  }
+
+  onEditProduct(product: any, modal: any) {
+    this.productForm.patchValue(product);
+    this.isEditingProduct = true;
+    this.modalService.open(modal);
+  }
   
+
+  deleteProduct(data: any) {
+    const prodId = data.prodId;
+    if (!prodId) {
+      alert('Error: Product ID is missing!');
+      return;
+    }
+    if (confirm('Are you sure you want to delete this product?')) {
+      this.switchService.deleteProductData(prodId).subscribe({
+        next: (response) => {
+          this.toastr.success(response.message);
+          this.getProductData();
+        },
+        error: (error) => {
+          this.toastr.error("id not yed");
+        }
+      });
+    }
+  }
+
+  productApplyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.productDataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  get f() {
+    return this.productForm.controls;
+  }
+  
+  getStatusClass(isActive: boolean): string {
+    return isActive ? "badge bg-success-transparent ps-3 fs-11 order-status complete " : "badge bg-danger-transparent ps-3 fs-11 order-status cancel";
+  }
+
+  getStatusText(isActive: boolean): string {
+    return isActive ? "Active" : "Inactive";
+  }
+
+  productGetSNo(index: number): number {
+    if (this.productPaginator && this.productPaginator.pageIndex !== undefined && this.productPaginator.pageSize !== undefined) {
+      return this.productPaginator.pageIndex * this.productPaginator.pageSize + index + 1;
+    }
+    return index + 1;
+  }
+
+  openProductPopup() {   
+      this.productForm.patchValue({prodId:this.generateProductId(),code:'',name:'',description:'',isActive:true});
+  }
+
+  openLg6(content10: any) {
+    this.modalService.open(content10, { scrollable: true, centered: true, });
+  }
 }
